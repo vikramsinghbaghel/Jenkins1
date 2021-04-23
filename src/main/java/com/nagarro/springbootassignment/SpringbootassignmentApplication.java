cc7c48719ef12f662c6305155248f7444d7ce166
vikramsinghbaghel <66490837+vikramsinghbaghel@users.noreply.github.com>
2021-04-23 15:48:00 +0530

    Add files via upload

---
+++ src/main/java/com/nagarro/springbootassignment/SpringbootassignmentApplication.java
@@ -0,0 +1,13 @@
+package com.nagarro.springbootassignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootassignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootassignmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagarro/springbootassignment/SpringbootassignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagarro.springbootassignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootassignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
